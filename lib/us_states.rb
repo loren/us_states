c25caa9ee401cc90a3a07a6d6d876005b29e4cda
Byron Anderson <byron@nationbuilder.com>
2012-09-28 16:59:45 -0700

    state name normalization

---
+++ lib/us_states.rb
@@ -0,0 +1,90 @@
+module State
+  class InvalidState < RuntimeError; end
+
+  CODES = {
+    'AL' => 'Alabama',
+    'AK' => 'Alaska',
+    'AS' => 'America Samoa',
+    'AZ' => 'Arizona',
+    'AR' => 'Arkansas',
+    'CA' => 'California',
+    'CO' => 'Colorado',
+    'CT' => 'Connecticut',
+    'DE' => 'Delaware',
+    'DC' => 'District of Columbia',
+    'FM' => 'Micronesia1',
+    'FL' => 'Florida',
+    'GA' => 'Georgia',
+    'GU' => 'Guam',
+    'HI' => 'Hawaii',
+    'ID' => 'Idaho',
+    'IL' => 'Illinois',
+    'IN' => 'Indiana',
+    'IA' => 'Iowa',
+    'KS' => 'Kansas',
+    'KY' => 'Kentucky',
+    'LA' => 'Louisiana',
+    'ME' => 'Maine',
+    'MH' => 'Islands1',
+    'MD' => 'Maryland',
+    'MA' => 'Massachusetts',
+    'MI' => 'Michigan',
+    'MN' => 'Minnesota',
+    'MS' => 'Mississippi',
+    'MO' => 'Missouri',
+    'MT' => 'Montana',
+    'NE' => 'Nebraska',
+    'NV' => 'Nevada',
+    'NH' => 'New Hampshire',
+    'NJ' => 'New Jersey',
+    'NM' => 'New Mexico',
+    'NY' => 'New York',
+    'NC' => 'North Carolina',
+    'ND' => 'North Dakota',
+    'OH' => 'Ohio',
+    'OK' => 'Oklahoma',
+    'OR' => 'Oregon',
+    'PW' => 'Palau',
+    'PA' => 'Pennsylvania',
+    'PR' => 'Puerto Rico',
+    'RI' => 'Rhode Island',
+    'SC' => 'South Carolina',
+    'SD' => 'South Dakota',
+    'TN' => 'Tennessee',
+    'TX' => 'Texas',
+    'UT' => 'Utah',
+    'VT' => 'Vermont',
+    'VI' => 'Virgin Island',
+    'VA' => 'Virginia',
+    'WA' => 'Washington',
+    'WV' => 'West Virginia',
+    'WI' => 'Wisconsin',
+    'WY' => 'Wyoming'
+  }
+
+
+  def self.member?(state)
+    CODES.member? normalize(state)
+  rescue InvalidState
+    false
+  end
+
+  def self.states
+    CODES.values
+  end
+
+  def self.normalize(state)
+    return state.upcase if CODES.member? state.upcase
+    return STATE_TO_CODE.fetch state.downcase
+  rescue KeyError
+    raise InvalidState
+  end
+
+  private
+
+  STATE_TO_CODE = CODES.inject({}) do |hash, code_state|
+    code, state = code_state
+    hash[state.downcase] = code
+    hash
+  end
+end
--- spec/us_states_spec.rb
+++ spec/us_states_spec.rb
@@ -1,7 +1,19 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'us_states'
 
-describe "UsStates" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe State do
+  describe ".normalize" do
+    specify { State.normalize("Alabama").should == "AL" }
+    specify { State.normalize("AL").should == "AL" }
+    specify { State.normalize("texas").should == "TX" }
+    specify do
+      expect { State.normalize("south") }.to raise_error(State::InvalidState)
+    end
+    specify { State.normalize("tx").should == "TX" }
+  end
+
+  describe ".member?" do
+    specify { State.member?("AL").should be_true }
+    specify { State.member?("XT").should be_false }
+    specify { State.member?("Alabama").should be_true }
   end
 end
